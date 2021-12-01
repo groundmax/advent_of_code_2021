5eb8701aaf050433707c20609030d1f24f0833e3
Maxim Lukin <maximlukin@MBP.local>
2021-12-02 01:35:51 +0300

    added solution of part1 day00

---
+++ day00/part1.py
@@ -0,0 +1,19 @@
+def main():
+    with open('input.txt') as f:
+        previous = int(f.readline())
+        counter = 0
+
+        for digit in f:
+
+            digit = int(digit)
+
+            if digit > previous:
+                counter += 1
+
+            previous = digit
+
+        print(counter)
+
+
+if __name__ == '__main__':
+    main()
