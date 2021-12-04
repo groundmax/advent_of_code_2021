3ed3051b39483a4c34fd38b82f4726615a3e1dd8
Maxim Lukin <maximlukin@MBP.local>
2021-12-04 14:47:08 +0300

    Added solutions for part 2 day00 and for part 1 day01

---
+++ day00/part2.py
@@ -0,0 +1,25 @@
+def main():
+    with open('input.txt') as f:
+        text = f.read()
+        array = [int(digit) for digit in text.splitlines()]
+        size = len(array)
+        i = 0
+        counter = 0
+        window_size = 3
+
+        while i < size - window_size:
+            idx_low_1, idx_high1 = i, i + window_size
+            idx_low_2, idx_high_2 = idx_low_1 + 1, idx_high1 + 1
+
+            previous_sum, next_sum = sum(array[idx_low_1:idx_high1]), sum(array[idx_low_2:idx_high_2])
+
+            if previous_sum < next_sum:
+                counter += 1
+
+            i += 1
+
+    print(counter)
+
+
+if __name__ == '__main__':
+    main()
---
+++ day01/part1.py
@@ -0,0 +1,16 @@
+def main():
+    with open('input.txt', 'r') as f:
+        text = f.read()
+        strings = [string for string in text.splitlines()]
+
+        commands = {}
+
+        for string in strings:
+            command, digit = string.split()
+            commands[command] = commands.get(command, 0) + int(digit)
+
+        print(max(commands['down'] - commands['up'], 0) * commands['forward'])
+
+
+if __name__ == '__main__':
+    main()
